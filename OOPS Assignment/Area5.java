8ecf391bb57db5a2c45f62307e598915fc5d15e1
unknown <padolekiran2001@gmail.com>
2025-01-24 21:21:36 +0530

    6 code complet node is still remining

---
+++ OOPS Assignment/Area5.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+// Create a class to calculate Area of circle with one data member to store the radius.
+// 	a. constructor - Initialize radius from user
+// 	b. calculateArea - to calculate area
+// 	3. display- to display area
+class Area{
+    private float PI = 3.14f;
+    int radius;
+    Area(int radius){
+        this.radius = radius;
+        System.out.print("This is default default constructor");
+    }
+    float calculateArea(){
+        return PI * radius * radius;
+    }
+    void display(){
+        System.out.println("Area of circle is " + calculateArea());
+    }
+}
+
+public class Area5 {
+    public static void main(String[] area){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of radius :");
+        int radius = sc.nextInt();
+
+        Area area1 = new Area(radius);
+        area1.display();
+    }
+    
+}
+
+
+// Enter value of radius :5
+// This is default default constructorArea of circle is 78.5
